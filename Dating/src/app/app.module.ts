8cc2b73095a29167b61218470fb06f60b8b85a79
JeevanGarg <luckyjeevangarg89>
2020-06-24 12:31:27 +0530

    First commit

---
+++ Dating/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
